72542ee8b9ba2decba52c1a80ff77a531e0d1755
sryang <sarang628@gmail.com>
2024-05-05 17:42:55 +0800

    update

--- ProfileScreenModule.kt
+++ ProfileScreenModule.kt
@@ -1,122 +1,31 @@
 package com.sarang.torang.di.profile_di
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavHostController
 import com.sarang.instagralleryModule.GalleryNavHost
-import com.sarang.torang.ProfileUiState
-
-import com.sarang.torang.compose.FeedListScreen
-import com.sarang.torang.compose.profile.MyProfileNavHost
-import com.sarang.torang.compose.profile.ProfileNavHost
+import com.sarang.torang.compose.profile._MyProfileScreen
 import com.sarang.torang.viewmodel.MyProfileViewModel
-import com.sarang.torang.viewmodel.ProfileViewModel
 
-
-@Composable
-fun ProfileScreen(
-    profileViewModel: ProfileViewModel = hiltViewModel(),
-    onSetting: () -> Unit,
-    onClose: (() -> Unit)? = null,
-    onEmailLogin: () -> Unit,
-    onReview: ((Int) -> Unit)? = null,
-    userId: Int,
-    onProfile: ((Int) -> Unit)? = null,
-    myFeed: @Composable (NavBackStackEntry) -> Unit,
-) {
-    val uiState by profileViewModel.uiState.collectAsState()
-
-    ProfileNavHost(
-        profileViewModel = profileViewModel,
-        onSetting = onSetting,
-        id = userId,
-        favorite = {
-            if (uiState is ProfileUiState.Success) {
-                FeedListScreen(/*favorite*/
-                    userId = (uiState as ProfileUiState.Success).id,
-                    onReview = onReview
-                )
-            }
-
-        },
-        wantToGo = {
-            if (uiState is ProfileUiState.Success) {
-                FeedListScreen(/*wantToGo*/
-                    userId = (uiState as ProfileUiState.Success).id,
-                    onReview = onReview
-                )
-            }
-        },
-        onClose = onClose,
-        onEmailLogin = onEmailLogin,
-        onProfile = onProfile,
-        myFeed = myFeed
-    )
-}
 
 @Composable
 fun MyProfileScreen(
     profileViewModel: MyProfileViewModel = hiltViewModel(),
     onSetting: () -> Unit,
-    navBackStackEntry: NavBackStackEntry?,
     onClose: (() -> Unit)? = null,
     onEmailLogin: () -> Unit,
-    onReview: ((Int) -> Unit)? = null,
     onProfile: ((Int) -> Unit)? = null,
     myFeed: @Composable (NavBackStackEntry) -> Unit
 ) {
-    val uiState by profileViewModel.uiState.collectAsState()
-
-    MyProfileNavHost(
+    _MyProfileScreen(
         myProfileViewModel = profileViewModel,
         onSetting = onSetting,
-        favorite = {
-            if (uiState is ProfileUiState.Success) {
-                FeedListScreen(/*favorite*/
-                    userId = (uiState as ProfileUiState.Success).id,
-                    onReview = onReview
-                )
-            }
-
-        },
-        wantToGo = {
-            if (uiState is ProfileUiState.Success) {
-                FeedListScreen(/*wantToGo*/
-                    userId = (uiState as ProfileUiState.Success).id,
-                    onReview = onReview
-                )
-            }
-        },
         galleryScreen = { onNext, onClose ->
             GalleryNavHost(onNext = onNext, onClose = { onClose.invoke() })
         },
         onClose = onClose,
         onEmailLogin = onEmailLogin,
         onProfile = onProfile,
-        myFeed = myFeed
-    )
-}
-
-fun provideProfileScreen(
-    navController: NavHostController,
-    myFeed: @Composable (NavBackStackEntry) -> Unit
-): @Composable () -> Unit = {
-    MyProfileScreen(
-        onSetting = { navController.navigate("settings") },
-        navBackStackEntry = null,
-        onClose = { navController.popBackStack() },
-        onEmailLogin = { navController.navigate("emailLogin") },
-        onReview = {
-            Log.d("__Main", "reviewId : ${it}")
-            navController.navigate("myFeed/${it}")
-        },
-        onProfile = {
-            navController.navigate("profile/${it}")
-        },
         myFeed = myFeed
     )
 }
