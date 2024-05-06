20fd6e64c91f44925294eddebc7e47be960c9351
sryang <sarang628@gmail.com>
2024-05-06 17:36:13 +0800

    update

--- ProfileScreenModule.kt
+++ ProfileScreenModule.kt
@@ -3,22 +3,25 @@ package com.sarang.torang.di.profile_di
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import com.sarang.instagralleryModule.GalleryNavHost
 import com.sarang.torang.compose._MyProfileScreenNavHost
 import com.sarang.torang.viewmodel.MyProfileViewModel
+import com.sarang.torang.viewmodel.ProfileViewModel
 
 
 @Composable
 fun MyProfileScreenNavHost(
-    profileViewModel: MyProfileViewModel = hiltViewModel(),
+    navController: NavHostController,
     onSetting: () -> Unit,
     onClose: (() -> Unit)? = null,
     onEmailLogin: () -> Unit,
     myFeed: @Composable (NavBackStackEntry) -> Unit,
-    onReview: ((Int) -> Unit)? = null,
+    onReview: ((Int) -> Unit)? = null
 ) {
     _MyProfileScreenNavHost(
-        myProfileViewModel = profileViewModel,
+        navController = navController,
         onSetting = onSetting,
         galleryScreen = { onNext, onClose ->
             GalleryNavHost(onNext = onNext, onClose = { onClose.invoke() })
