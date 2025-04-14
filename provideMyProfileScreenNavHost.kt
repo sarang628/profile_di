13e022a3243f96667db4e74171c52f3b8db0ef2c
sryang <sarang628@gmail.com>
2025-04-14 22:14:47 +0900

    update

--- provideMyProfileScreenNavHost.kt
+++ provideMyProfileScreenNavHost.kt
@@ -4,34 +4,35 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.RootNavController
+import com.sarang.torang.di.chat_di.ChatActivity
 import com.sarang.torang.di.main_di.ProvideMyFeedScreen
 import com.sarang.torang.di.main_di.provideCommentBottomDialogSheet
+import com.sarang.torang.di.video.provideVideoPlayer
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-internal fun provideMyProfileScreenNavHost(rootNavController: RootNavController, videoPlayer: @Composable (url: String, isPlaying: Boolean, onVideoClick: () -> Unit) -> Unit, onMessage: (Int) -> Unit): @Composable () -> Unit =
+internal fun provideMyProfileScreenNavHost(rootNavController: RootNavController): @Composable () -> Unit =
     {
+        val context = LocalContext.current
         val profileNavController = rememberNavController() // 상위에 선언하면 앱 죽음
         MyProfileScreenNavHost(
             navController = profileNavController,
-            onSetting = { rootNavController.settings() },
-            onEmailLogin = { rootNavController.emailLogin() },
-            onReview = {
-                Log.d("__Main", "MyProfileScreen onReview reviewId : ${it}")
-                profileNavController.navigate("myFeed/${it}")
-            },
-            onClose = { profileNavController.popBackStack() },
+            onSetting = rootNavController::settings,
+            onEmailLogin = rootNavController::emailLogin,
+            onReview = { profileNavController.navigate("myFeed/${it}") },
+            onClose = profileNavController::popBackStack,
             myFeed = {
                 ProvideMyFeedScreen(
                     rootNavController = rootNavController,
                     navController = profileNavController,
                     navBackStackEntry = it,
-                    videoPlayer = videoPlayer,
+                    videoPlayer = provideVideoPlayer(),
                     commentBottomSheet = provideCommentBottomDialogSheet(rootNavController)
                 )
             },
-            onMessage = onMessage
+            onMessage = { ChatActivity.go(context, it) }
 
         )
     }
