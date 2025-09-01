4c1d9e5d9fdd15774eac7c016e8b927724e78c2f
sryang <sarang628@gmail.com>
2025-09-01 08:05:57 +0800

    change package

--- ProfileServiceModule.kt
+++ ProfileServiceModule.kt
@@ -5,7 +5,7 @@ import com.sarang.torang.Feed
 import com.sarang.torang.ProfileUiState
 import com.sarang.torang.usecase.profile.IsLoginUseCase
 import com.sarang.torang.usecase.profile.ProfileService
-import com.sarang.torang.data.dao.LoggedInUserDao
+import com.sarang.torang.core.database.dao.LoggedInUserDao
 import com.sarang.torang.di.repository.repository.impl.ProfileRepositoryImpl
 import com.sarang.torang.repository.EditProfileRepository
 import dagger.Module
