0530fda3b6c995585521a3b068c4408cebc0f167
sryang <sarang628@gmail.com>
2026-01-17 16:23:24 +0900

    hoisting MyFeedListViewModel for refresh

--- ProfileScreenModule.kt
+++ ProfileScreenModule.kt
@@ -7,24 +7,27 @@ import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.sarang.torang.compose.profile.LocalProfileImage
 import com.sarang.torang.compose.profile._MyProfileScreenNavHost
 import com.sarang.torang.di.image.provideTorangAsyncImage
+import com.sarang.torang.viewmodel.profile.MyFeedListViewModel
 
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun MyProfileScreenNavHost(
-    navController               : NavHostController = rememberNavController(),
-    onSetting                   : () -> Unit        = {},
-    onClose                     : () -> Unit        = {},
-    onEmailLogin                : () -> Unit        = {},
-    onReview                    : (Int) -> Unit     = {},
-    onMessage                   : (Int) -> Unit     = {},
-    myProfileBackgroundColor    : Color             = Color.Transparent,
-    contentWindowInsets         : WindowInsets      = ScaffoldDefaults.contentWindowInsets
+    navController               : NavHostController     = rememberNavController(),
+    feedListViewModel           : MyFeedListViewModel   = hiltViewModel(),
+    onSetting                   : () -> Unit            = {},
+    onClose                     : () -> Unit            = {},
+    onEmailLogin                : () -> Unit            = {},
+    onReview                    : (Int) -> Unit         = {},
+    onMessage                   : (Int) -> Unit         = {},
+    myProfileBackgroundColor    : Color                 = Color.Transparent,
+    contentWindowInsets         : WindowInsets          = ScaffoldDefaults.contentWindowInsets
 ) {
     CompositionLocalProvider(LocalProfileImage provides {provideTorangAsyncImage().invoke(
         it.modifier,
@@ -33,20 +36,21 @@ fun MyProfileScreenNavHost(
         it.progressSize,
         it.contentScale)}) {
         _MyProfileScreenNavHost(
-            navController = navController,
-            onSetting = onSetting,
-            galleryScreen = { onNext, onClose ->
-                /*GalleryNavHost(
-                    onNext = onNext,
-                    onClose = { onClose.invoke() },
-                    onBack = { onClose.invoke() })*/
-            },
-            onClose = onClose,
-            onEmailLogin = onEmailLogin,
-            onReview = onReview,
-            onMessage = onMessage,
-            myProfileBackgroundColor = myProfileBackgroundColor,
-            contentWindowInsets = contentWindowInsets
+            navController               = navController,
+            feedListViewModel           = feedListViewModel,
+            onSetting                   = onSetting,
+            galleryScreen               = { onNext, onClose ->
+                                            /*GalleryNavHost(
+                                                onNext = onNext,
+                                                onClose = { onClose.invoke() },
+                                                onBack = { onClose.invoke() })*/
+                                           },
+            onClose                     = onClose,
+            onEmailLogin                = onEmailLogin,
+            onReview                    = onReview,
+            onMessage                   = onMessage,
+            myProfileBackgroundColor    = myProfileBackgroundColor,
+            contentWindowInsets         = contentWindowInsets
         )
     }
 }
